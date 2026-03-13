db6bd43208fb624e0250180e1e5e5185e37428d5
ROGpin81 <roger.pinedab@gmail.com>
2026-03-13 15:20:37 -0600

    Dejar de versionar frontend/.next

--- frontend/.next/dev/server/chunks/ssr/frontend__next-internal_server_app_(general)_registro_page_actions_7cde1b5d.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/frontend/.next-internal/server/app/(general)/registro/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=frontend__next-internal_server_app_%28general%29_registro_page_actions_7cde1b5d.js.map
--- frontend/.next/dev/static/chunks/frontend_src_app_(general)_login_page_tsx_1097bcdc._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/9e883_b16a9672._.js",
-  "static/chunks/frontend_src_b64ab506._.js"
-],
-    source: "dynamic"
-});
