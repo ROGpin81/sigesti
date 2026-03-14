141eb456866ff0c6d8c161a7d5823d0f645c2177
ROGpin81 <roger.pinedab@gmail.com>
2026-03-14 14:10:42 -0600

    Quitar espacios entre Apis (Solo cambio de forma)

--- backend/routes/ticketsRoute.js
+++ backend/routes/ticketsRoute.js
@@ -4,11 +4,8 @@ const router = express.Router();
 const ticketsController = require("../controllers/ticketsController");
 
 router.post("/", ticketsController.crearTicket);
-
 router.get("/", ticketsController.obtenerTickets);
-
 router.get("/:id", ticketsController.obtenerTicketPorId);
-
 router.put("/:id", ticketsController.actualizarTicket);
 
 module.exports = router;
