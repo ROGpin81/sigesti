5504a671378a1dd9ee49601b483d8dab58f3b197
ROGpin81 <roger.pinedab@gmail.com>
2026-03-19 21:22:48 -0600

    Create Apis collections e integración Frontend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const sequelize = require("./db/connection");
 const registroRoute = require("./routes/registroRoute");
 const loginRoute = require("./routes/loginRoute");
@@ -7,20 +8,34 @@ const usersRoute = require("./routes/usersRoute");
 const authRoute = require("./routes/authRoute");
 const ticketsRoute = require("./routes/ticketsRoute");
 const collectionsRoute = require("./routes/collectionsRoute");
+const filesRoutes = require("./routes/files.routes");
 
 const cors = require("cors");
 const authMiddleware = require("./middlewares/auth").authMiddleware;
 
 const app = express();
 
-app.use(express.json());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
-app.use(
+app.use(cors(corsOptions));
+
+/*app.use(
     cors({
         origin: "http://localhost:3000",
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: false,
     }),
+);*/
+
+app.use(express.json());
+
+app.use(
+  "/uploads",
+  express.static(path.join(__dirname, "src/uploads"))
 );
 
 // APIs públicas
@@ -37,6 +52,7 @@ app.get("/prueba", authMiddleware, (req, res) => {
 app.use("/users", authMiddleware, usersRoute);
 app.use("/tickets", authMiddleware, ticketsRoute);
 app.use("/collections", authMiddleware, collectionsRoute);
+app.use("/api", filesRoutes);
 
 const PORT = process.env.PORT;
 
